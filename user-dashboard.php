d2bc80874337669edbc2c85e08fb9ec3f1d90fba
ferdi666 <81407511+ferdi666@users.noreply.github.com>
2021-06-23 00:24:19 +0800

    updated user-dashboard

--- user-dashboard.php
+++ user-dashboard.php
@@ -23,25 +23,39 @@
 <body>
   <!--MENU-->
   <div class="sidenav">
-    <div class="donate">
-      <a href="user-dashboard-donate.html" id="first-sidebar" class="a"><img src="icons/menu/medical-center.png">Donate</a>
+    <div class="float-container">
+        <div class="float-child">
+           <div class="icon"><img src="icons/Hospital-Dashboard-Menu/rh-.png"></div>
+        </div>
+        <div class="float-child">
+          <div class="prof-text">
+            <p><b><?php echo $_SESSION['Hospital_Name']?></b></p>
+            <small>Davao City</small>
+          </div>
+        </div>
+
     </div>
-    <div class="request">
-      <a href="user-dashboard-request.html" class="a"><img src="icons/menu/volunteer.png">Request</a>
+
+    <div class="menu">
+      <div class="donate">
+        <a href="user-dashboard-donate.html" id="first-sidebar" class="a"><img src="icons/menu/medical-center.png">Donate</a>
+      </div>
+      <div class="request">
+        <a href="user-dashboard-request.html" class="a"><img src="icons/menu/volunteer.png">Request</a>
+      </div>
+      <div class="search">
+        <a href="#" id="third-sidebar" class="a"><img src="icons/menu/blood-transfusion.png">Search</a>
+      </div>
+      <div class="sidebar-bottom">
+        <table>
+          <th><a href="login.html" id="log-out">Log-out</a></th>
+        </table>
+      </div>
     </div>
-    <div class="search">
-      <a href="#" id="third-sidebar" class="a"><img src="icons/menu/blood-transfusion.png">Search</a>
-    </div>
-    <div class="sidebar-bottom">
-      <table>
-        <th><a href="php/logout.php" id="log-out">Log-out</a></th>
-      </table>
-    </div>
-  </div>
+  </div>s
 
   <!--Red banner-->
   <div class="header">
-    <h1>Hello, <?php echo $_SESSION['User_Name']?></h1> <!-- Print user name in the user-dashboard -->
   </div>
 
   <!--Title of table w/ Search-->
@@ -55,32 +69,43 @@
   <!--Table-->
   <div class="content">
     <table class="table-content" style="width: 90%">
-    <tr>
-          <th>Hospital</th>
-          <th>Location</th>
-          <th>Contact Number</th>
-        </tr>
-        <tr>
-             <?php
-        $conn = mysqli_connect("localhost","root","","blood_donation_sys");
-        if($conn->connect_error){ 
-          die("Connection failed:".$conn->connect_error); 
-        } 
-        $sql = "SELECT Hospital_Name,
-        Hospital_Address, Hospital_Contact_Number FROM hospital"; 
-        $result = $conn->query($sql);
-
-        if($result->num_rows>0){
-          while($row=$result->fetch_assoc()){
-            echo "<tr><td>".$row["Hospital_Name"]."</td><td>".$row["Hospital_Address"]."</td><td>".$row["Hospital_Contact_Number"]."</td></tr>";
-          }
-          echo"</table>";
-        }
-        else{
-          echo "0 result";
-        }
-        $conn->close();
-        ?>
+      <tr>
+        <th>Hospital</th>
+        <th>Location</th>
+        <th>Total Blood Bags</th>
+        <th>Status</th>
+      </tr>
+      <tr>
+        <td>Lorem Ipsum Hospital</td>
+        <td>Westminster, United States</td>
+        <td>9</td>
+        <td>Available</td>
+      </tr>
+      <tr>
+        <td>Lorem Ipsum Hospital</td>
+        <td>Westminster, United States</td>
+        <td>9</td>
+        <td>Available</td>
+      </tr>
+      <tr>
+        <td>Lorem Ipsum Hospital</td>
+        <td>Westminster, United States</td>
+        <td>9</td>
+        <td>Available</td>
+      </tr>
+      <tr>
+        <td>Lorem Ipsum Hospital</td>
+        <td>Westminster, United States</td>
+        <td>9</td>
+        <td>Available</td>
+      </tr>
+      <tr>
+        <td>Lorem Ipsum Hospital</td>
+        <td>Westminster, United States</td>
+        <td>9</td>
+        <td>Available</td>
+      </tr>
+    </table>
   </div>
 </body>
 </html>
