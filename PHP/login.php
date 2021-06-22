6f40cab925a6f1db13c520bd730e0064301202a4
ferdi666 <81407511+ferdi666@users.noreply.github.com>
2021-06-22 23:24:20 +0800

    updated login.php and signup.php

--- PHP/login.php
+++ PHP/login.php
@@ -17,7 +17,7 @@
     function send_alert_error($message){
         echo '<script language="javascript">';
         echo 'alert("',$message,'");';
-        echo "window.location.href='../bloodbank_system-main/login.html';";
+        echo "window.location.href='../login.html';"; //healjksad//
         echo '</script>';
     }
 
