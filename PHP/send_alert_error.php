9d2a54c92a07b3fb34dc83cd727d372a6d333d94
ferdi666 <81407511+ferdi666@users.noreply.github.com>
2021-06-27 08:40:22 +0800

    changed login.html to index.html

--- PHP/send_alert_error.php
+++ PHP/send_alert_error.php
@@ -3,7 +3,7 @@
   function send_alert_error($message){
         echo '<script language="javascript">';
         echo 'alert("',$message,'");';
-        echo "window.location.href='../bloodbank_system-main/login.html';";
+        echo "window.location.href='../bloodbank_system-main/index.html';";
         echo '</script>';
     }
 
